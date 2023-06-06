34d1f2c1f71eb1c94f96ff139fda7ebde3bb4f97
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-06 22:41:02 +0300

    check last digit of integer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDigit = number%10 if number >= 0 else number % -10
-message = ""
-if lastDigit == 0:
-   message ="and is 0"
-elif lastDigit < 6:
-    message = "and is less than 6 and not 0"
+last_dig = number % 10 if number >= 0 else number % -10
+message = ''
+if (last_dig == 0):
+    message = 'and is 0'
+elif (last_dig < 6):
+    message = 'and is less than 6 and not 0'
 else:
-    message = "and is greater than 5"
-print(f"Last digit of {number:d} is {lastDigit:d} {message:s}")
+    message = 'and is greater than 5'
+print(f'Last digit of {number:d} is {last_dig:d} {message:s}')
