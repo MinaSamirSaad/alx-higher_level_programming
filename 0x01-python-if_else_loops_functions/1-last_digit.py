6ed20c053d45abfaf4d189749c1cc5a51d3139ac
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-06 22:35:12 +0300

    check last digit of integer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ lastDigit = number%10 if number >=0 else number % -10
 message = ""
 if lastDigit == 0:
    message =" and is 0"
-elif lastDigit >= 6:
-    message = " and is greater than 5"
-else:
+elif lastDigit < 6:
     message = " and is less than 6 and not 0"
+else:
+    message = " and is greater than 5"
 print(f"Last digit of {number:d} is {lastDigit:d}{message:s}")
