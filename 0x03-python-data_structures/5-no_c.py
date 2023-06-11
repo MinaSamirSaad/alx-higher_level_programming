ceb22a121e9002f95a885c18ac77586402d9e224
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-11 23:57:07 +0300

    function that removes all characters c and C from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    copy_string = ""
     for i in my_string:
-        if(i == 'c' or i == 'C'):
-            i = ''
-    return my_string
+        if i != 'c' and i != 'C':
+            copy_string += i
+    return copy_string
