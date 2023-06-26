e8df64e5eb26ae233fedf18be19683ef601c7e86
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-27 01:04:31 +0300

    three C functions that print some basic info about Python lists, Python bytes an Python float objects

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,68 @@
+#include <Python.h>
+#include <stdio.h>
+/**
+ * print_python_list - print basic info about Python lists
+ * @p: Python list
+ */
+void print_python_list(PyObject *p)
+{
+Py_ssize_t size, i;
+PyObject *item;
+
+printf("[*] Python list info\n");
+printf("[*] Size of the Python List = %zd\n", PyList_Size(p));
+printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+
+for (i = 0; i < PyList_Size(p); i++)
+{
+item = PyList_GetItem(p, i);
+printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+}
+}
+/**
+ * print_python_bytes - print basic info about Python bytes
+ * @p: Python bytes
+ */
+void print_python_bytes(PyObject *p)
+{
+Py_ssize_t size, i;
+char *str;
+
+printf("[.] bytes object info\n");
+if (!PyBytes_Check(p))
+{
+printf("  [ERROR] Invalid Bytes Object\n");
+return;
+}
+
+size = PyBytes_Size(p);
+str = PyBytes_AsString(p);
+
+printf("  size: %zd\n", size);
+printf("  trying string: %s\n", str);
+
+printf("  first %zd bytes:", (size < 10 ? size + 1 : 10));
+for (i = 0; i < size && i < 10; i++)
+{
+printf(" %02x", (unsigned char)str[i]);
+}
+printf("\n");
+}
+/**
+ * print_python_float - print basic info about Python float
+ * @p: Python float number
+ */
+void print_python_float(PyObject *p)
+{
+double value;
+
+printf("[.] float object info\n");
+if (!PyFloat_Check(p))
+{
+printf("  [ERROR] Invalid Float Object\n");
+return;
+}
+
+value = PyFloat_AsDouble(p);
+printf("  value: %g\n", value);
+}
