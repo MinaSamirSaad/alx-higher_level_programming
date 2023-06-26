77a536eb6092316c83400589f22ac48ad0542bd5
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-27 00:50:17 +0300

    function that executes a function safely.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return fct(args)
+        return fct(*args)
     except Exception as error:
         print('Exception: {}'.format(error), file=sys.stderr)
         return None
