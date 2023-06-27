9eee2c84c129e1368f09e7ac1e2f6721d5b7bd09
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-28 00:42:23 +0300

    class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' module 0-saqure.py creates square class '''
+
+
+class Square:
+    ''' sqaure class doc '''
+    def __init__(self, size):
+        ''' constructor doc '''
+        self.__size = size
