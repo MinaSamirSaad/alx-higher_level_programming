19dd67ea4b017bbe8349f8512eda172e5f702106
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-28 01:49:51 +0300

    class Square that defines a square by: (based on 4-square.py)

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,7 +31,7 @@ class Square:
         return self.__size ** 2
     
     def my_print(self):
-        ''' prints in stdout the square with the character # '''
+        ''' the square with the character # '''
         if self.__size == 0:
             print()
         else:
