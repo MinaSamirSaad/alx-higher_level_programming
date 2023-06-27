134bd69f6274949c491ee52b2b49ffa56f0d9433
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-28 01:07:30 +0300

    class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' module 0-saqure.py creates square class '''
+
+
+class Square:
+    ''' sqaure class doc '''
+    def __init__(self, size=0):
+        ''' constructor doc '''
+        if not isinstance(size,int):
+            raise TypeError("size must be an intege")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
