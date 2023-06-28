619a9bb97b1b024e46582b4b0fa0698720faeb29
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-06-29 02:16:20 +0300

    class Square that defines a square by: (based on 6-square.py)

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+''' module 0-saqure.py creates square class '''
+
+
+class Square:
+    ''' sqaure class doc '''
+    def __init__(self, size=0, position=(0, 0)):
+        ''' constructor doc '''
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        ''' get size '''
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        ''' size settter '''
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def position(self):
+        ''' get position '''
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 \
+                or any(not isinstance(n, int) for n in value) \
+                or any(n < 0 for n in value):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        ''' compute area of square '''
+        return self.__size ** 2
+
+    def __str__(self):
+        ''' replace __str__'''
+        if (self.__size == 0):
+            return '\n'
+        square = ''
+        square += '\n' * self.__position[1]
+        for i in range(self.__size):
+            square += ' ' * self.__position[0]
+            square += '#' * self.__size
+            if i + 1 < self.__size:
+                square += '\n'
+        return square
