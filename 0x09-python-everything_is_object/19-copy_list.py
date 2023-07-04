454401301a9b782eb0676b1c52c3e90b2ebdd903
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-07-05 00:39:59 +0300

    solutions

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def copy_list(list_):
-    return list_.copy() if (type(list_) is list or type(list_) is dict) else list_
+    return list_.copy() if (isinstance(list_, list) or isinstance(list_, dict)) else list_
