22f5c8b5fc6afa04025efad01bf7950ebf616a79
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-07-07 22:58:01 +0300

    update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+"""
+Text indentation module
+
+This module prints a text with 2 new lines after each of these characters:
+., ? and :. The text must be a string.
+Otherwise raise a TypeError exception with custom error messages.
+"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
     """
-    if not isinstance(text, str):
+    Pints a text with 2 new lines after each of these characters:., ? and :
+    Arguments:
+    @text: text to indentate
+
+    Returns:
+    Nothing; only prints the new text, otherwise raise a TypeError
+    when text is not a string
+
+    Example:
+    >>> text_indentation("Hello.World")
+    Hello.
+
+    World
+    """
+    if type(text) is not str or text is None or len(text) < 0:
         raise TypeError("text must be a string")
-
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
+    text = text.strip()
+    flag = False
+    symbols = ['.', '?', ':']
+    for c in text:
+        if c == ' ' and flag is True:
             continue
-        c += 1
+            flag = False
+        if c in symbols:
+            print(c)
+            print()
+            flag = True
+        else:
+            print(c, end="")
+            flag = False
