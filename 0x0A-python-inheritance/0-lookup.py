4759e8d7c3af41f38172d16ef896afe2856781ab
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-07-10 21:55:52 +0300

    [200~'''function that returns the list of available attributes and methods of an object'''

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''function that returns the list of available attributes and methods of an object'''
+
+def lookup(obj):
+    """
+    Arguments:
+    obj: object to take the attributes
+    Return:
+    The list of all available attributes
+    """
+    return (dir(obj))
