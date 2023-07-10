cb1da97b851ef9cc0145b0c9c2c14539b1cd7739
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-07-10 22:24:24 +0300

    function to test if object inhert from class or not

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-'''
-unction that returns True if the object is exactly an instance of the specified class 
-otherwise False
-'''
+"""
+Function that verify if an object is a exactly an instance of the specified
+class, if it's true return True, otherwise False
+"""
 
 
 def is_same_class(obj, a_class):
-    '''
+    """
     Arguments:
-    obj: the object
-    a_class: the class
+    @obj: object to verify
+    @a_class: class of the object to verify
     Return:
-    True if the object is exactly an instance of the specified class 
-    otherwise False
-    '''
-    if  obj and  a_class:
-        return  (isinstance(obj, a_class))
-    return (False)
+    True if the object is exactly an instance of the class, otherwise False
+    """
+    if type(obj) is a_class:
+        return (True)
+    else:
+        return (False)
