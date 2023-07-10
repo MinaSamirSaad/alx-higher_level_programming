95f5ba8199b5ff623f722ee0d4b1038d4c655fd3
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-07-11 00:44:01 +0300

    function that adds a new attribute to an object if it’s possible

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,7 +6,6 @@ BaseGeometry module
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-
 class Rectangle(BaseGeometry):
     """ Class Rectangle """
     def __init__(self, width, height):
