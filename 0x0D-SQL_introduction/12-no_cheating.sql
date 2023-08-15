72601cac7ead6a4e82481741c7018eed8cec179e
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-08-16 02:29:29 +0300

    solutions

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,4 @@
 -- script that updates the score of Bob to 10 in the table second_table
-UPDATE hbtn_0c_0.second_table SET id = 10 WHERE name = "Bob";
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
