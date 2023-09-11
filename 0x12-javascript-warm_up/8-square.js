4d4388a93cb61cc6fe469a27d04edd9cf90eb783
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-11 17:40:36 +0300

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
-const argv = require('process').argv;
-const num = parseInt(argv[2], 10);
-if (num) {
-  if (num > 0) {
-    let mes;
-    for (let i = 0; i < num; i++) {
-      mes = '';
-      for (let j = 0; j < num; j++) {
-        mes += 'x'
-      }
-      console.log(mes);
-    }
-  }
-} else {
+if (!process.argv[2] || !parseInt(process.argv[2])) {
   console.log('Missing size');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('X'.repeat(process.argv[2]));
+  }
 }
