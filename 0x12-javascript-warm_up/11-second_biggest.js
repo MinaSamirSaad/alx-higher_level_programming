0ce74fce94ad089202f97f5cc8c797669fa909a8
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-11 21:25:09 +0300

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ let num;
 if (process.argv.length < 4) {
   num = 0;
 } else {
-  num = parseInt(process.argv.slice(2).sort((a, b)=>a - b).reverse()[1]);
+  num = parseInt(process.argv.slice(2).sort((a, b) => a - b).reverse()[1]);
 }
 console.log(num);
