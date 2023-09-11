b689073594f4a22d59378d6ea11423cc7cc6b3b2
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-11 17:35:40 +0300

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,11 +3,12 @@ const argv = require('process').argv;
 const num = parseInt(argv[2], 10);
 if (num) {
   if (num > 0) {
-    let mes = '';
-    for (let j = 0; j < num; j++) {
-      mes += 'x';
-    }
+    let mes;
     for (let i = 0; i < num; i++) {
+      mes = '';
+      for (let j = 0; j < num; j++) {
+        mes += 'x'
+      }
       console.log(mes);
     }
   }
