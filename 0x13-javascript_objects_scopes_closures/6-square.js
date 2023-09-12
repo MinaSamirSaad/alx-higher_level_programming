a263e9a04e4ebce1a536857d53f7ad7b8ede79b4
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-12 23:08:41 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Sq = require('./5-square');
 module.exports = class Square extends Sq {
-  charPrint (c = 'x') {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.width; i++) {
       console.log(c.repeat(this.height));
     }
