e89f0780e39dc8c6557fe589f851b1ec38338b24
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-13 00:20:00 +0300

    task 102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+const file3 = process.argv[4];
+const contentFile1 = fs.readFileSync(file1, 'utf8');
+const contentFile2 = fs.readFileSync(file2, 'utf8');
+const ContentFile3 = `${contentFile1}\n${contentFile2}`;
+fs.writeFileSync(file3, ContentFile3);
