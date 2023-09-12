2de7778d4e646223c069a6db78921ed372bd4063
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-12 22:16:31 +0300

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
+
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (h > 0 && w > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('x'.repeat(this.width));
-      }
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 };
