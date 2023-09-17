8426b1312c83da6e0a9a97f61ef146ebdd15fd8e
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-17 15:52:38 +0300

    task 1

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    userName = argv[1]
+    password = argv[2]
+    dbName = argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=userName,
+                           passwd=password, db=dbName, charset="utf8")
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM states WHERE BINARY name = \
+                '{argv[4]}' ORDER BY id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
