cffc0f0edb7f029132a8ef7e831e68dd640f1ada
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-29 01:54:08 +0300

    script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response A header variable X-School-User-Id must be sent with the value 98

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-# A header variable X-School-User-Id must be sent with the value 98
 curl -s "$1" -X GET -H "X-School-User-Id:98"
