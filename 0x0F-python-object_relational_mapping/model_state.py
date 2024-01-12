56f641ef6a65e9b92eea2768847858504c567991
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-01-12 14:51:29 +0400

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -23,5 +23,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    # Establish a one-to-many relationship with City
-    cities = relationship("City", back_populates="state")
+
