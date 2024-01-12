80cbc778c0591c6241b16e4ac8d9e57d6a98b376
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-01-12 23:15:37 +0200

    update

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
+
 class City(Base):
     """
     City class representing the 'cities' table in the database.
@@ -22,4 +23,3 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,7 +15,7 @@ Base = declarative_base()
 class State(Base):
     """
     State class that inherits from Base
-    
+
     Attributes:
     - id (int): Auto-generated unique integer, primary key.
     - name (str): String of 128 characters, cannot be null.
@@ -23,4 +23,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
