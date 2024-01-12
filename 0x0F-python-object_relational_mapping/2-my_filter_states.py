c009fbf8f2202b073dd77c3f5a1f0e631a857965
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-01-12 23:25:10 +0200

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
         "SELECT * FROM states where name='{}' order by id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
