8ceea627995c2660bec6ae4f66fb75b3a91f29ed
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-01-26 23:12:38 +0200

    Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+Python script that takes in a URL and an email address
+sends a POST request to the passed URL with the email as a parameter
+and finally displays the body of the response.
+'''
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    res = requests.post(argv[1],data={'email': argv[2]},timeout=5)
+    print(res.text)
