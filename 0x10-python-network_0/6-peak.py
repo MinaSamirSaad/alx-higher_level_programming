15a62aa00e1d3868a00d9c93dd5628903d4215cb
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-01-26 02:56:28 +0200

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,7 +10,7 @@ def find_peak(list_of_integers):
     '''
     if len(list_of_integers) == 0:
         return None
-    elif len(list_of_integers) <= 3:
+    elif len(list_of_integers) < 3:
         return max(list_of_integers)
 
     return max(list_of_integers[1:-1])
