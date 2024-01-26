af49152b84592b71cbad2718a98360d9669157f1
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-01-26 23:13:13 +0200

    Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ and finally displays the body of the response.
 if __name__ == '__main__':
     import requests
     from sys import argv
-    res = requests.post(argv[1],data={'email': argv[2]},timeout=5)
+    res = requests.post(argv[1], data={'email': argv[2]}, timeout=5)
     print(res.text)
