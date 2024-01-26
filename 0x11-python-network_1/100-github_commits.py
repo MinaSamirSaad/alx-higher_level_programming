d2f01d3407c52f50cd68c2a9d6691f78b0c36aea
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-01-27 00:30:15 +0200

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+script that takes 2 arguments in order to solve this challenge.
+The first argument will be the repository name
+The second argument will be the owner name
+'''
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    res = requests.get(url)
+    commits = res.json()
+    for commit in commits[:10]:
+        print("{}: {}".format(commit.get('sha'),
+                              commit.get('commit').get('author').get('name')))
