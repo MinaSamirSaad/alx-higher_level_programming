2505056953419496a6cf434411944d92225dd6b3
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-21 02:26:59 +0200

    writeme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const file = process.argv[2];
+const data = process.argv[3];
+fs.writeFile(file, data, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
