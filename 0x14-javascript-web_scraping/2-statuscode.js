fba3d18097c7ad17589ecca497d0d0d3978eb53d
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-21 02:33:02 +0200

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  console.log('code:', response.statusCode);
+});
